c94a73b7c118d008024d440659fd0205c7d46396
senhoang <nguyenhoangsen198@gmail.com>
2021-08-29 23:35:41 +0700

    husky e

--- Blog/src/routes/index.js
+++ Blog/src/routes/index.js
@@ -3,8 +3,8 @@ const siteRouter = require('./site');
 
 function route(app) {
   app.use("/news", 
-      newsRouter);
-    app.use('/', siteRouter);
+                  newsRouter);
+      app.use('/', siteRouter);
 }
 
 module.exports = route;
