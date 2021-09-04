fcc567f857dafe1e6c3febe15ccf1332890ee8c5
senhoang <nguyenhoangsen198@gmail.com>
2021-09-04 22:46:54 +0700

    import dependencies

--- do_not_touch_face/src/App.js
+++ do_not_touch_face/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import {Howl, Howler} from 'howler';
+import soundURL from './assets/hey_sondn.mp3'
 
 const tf = require('@tensorflow/tfjs');
-const knnClassifier = require('./node_modules/@tensorflow-models/knn-classifier/dist/knn-classifier');
-
+const knnClassifier = require('@tensorflow-models/knn-classifier');
 const mobilenet = require('@tensorflow-models/mobilenet');
 
+var sound = new Howl({
+  src: ['soundURL']
+});
 
+sound.play();
 
 function App() {
   return (
