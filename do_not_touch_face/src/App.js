1f782e4fceef2e13ee4f9c32206453f40e2fcec4
senhoang <nguyenhoangsen198@gmail.com>
2021-09-04 20:34:53 +0700

    base

--- do_not_touch_face/src/App.js
+++ do_not_touch_face/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
 
+const tf = require('@tensorflow/tfjs');
+const knnClassifier = require('./node_modules/@tensorflow-models/knn-classifier/dist/knn-classifier');
+
+const mobilenet = require('@tensorflow-models/mobilenet');
+
+
+
 function App() {
   return (
-    <div className="App">
+    <div className="main">
+      <video
+        className="video"
+        autoPlay
+      >
+      </video>
+
+    <div className="control">
+      <button className="btn">Train 1</button>
+      <button className="btn">Train 2</button>
+      <button className="btn">Run</button>
+    </div>
     </div>
   );
 }
